d64d909cc13249cb9984fd8f74d2fe06235817bb
Rasmus Holm <rasmus.holm@modularfinance.se>
2016-11-18 12:18:58 +0100

    recatoring

--- app/scripts.babel/name-to-class.js
+++ app/scripts.babel/name-to-class.js
@@ -1,79 +1,150 @@
-var fileNamesToClass = {
-  '.gitignore': 'github',
-  '.gitattributes': 'github',
-  '.gitmodules': 'github',
-  'bower.json': 'bower',
-  'package.json': 'npm',
-  'nginx.conf': 'nginx',
-  '.babelrc': 'babel',
-  '.bowerrc': 'bower',
-  'bowerfile': 'bower',
-  'Vagrantfile': 'vagrant',
-  'docker-compose.extra.yml': 'docker',
-  'docker-compose.yml': 'docker',
-  '.dockerignore': 'docker',
-  'dockerfile': 'docker',
-  'dockerfile.extra': 'docker',
-  'docker-sync.yml': 'docker',
-  'gulpfile.js': 'gulp',
-  'gruntfile.js': 'grunt',
-};
 
-var extentionToClass = {
-  'html': 'html5',
-  'html.erb': 'html5',
-  'slim': 'html5',
-  'css': 'css3',
-  'less': 'css3',
-  'scss': 'sass',
-  'json': 'jsonld',
-  'geojson': 'jsonld',
-  'jsonld': 'jsonld',
-  'pub': 'key',
-  'png': 'image',
-  'jpg': 'image',
-  'svg': 'image',
-  'babel.js': 'babel',
-  'woff2': 'font',
-  'woff': 'font',
-  'ttf': 'font',
-  'ttc': 'font',
-  'otf': 'font',
-  'eot': 'font',
-  'accda': 'access',
-  'accde': 'access',
-  'accdb': 'access',
-  'accdr': 'access',
-  'accdt': 'access',
-  'adn': 'access',
-  'laccdb': 'access',
-  'mdw': 'access',
-  'asax' : 'asp',
-  'ascx' : 'asp',
-  'ashx' : 'asp',
-  'asmx' : 'asp',
-  'aspx' : 'asp',
-  'axd' : 'asp',
-  'hbs' : 'mustache',
-  'hs': 'haskell',
-  'clj': 'clojure',
-  'hpp': 'cpp',
-  'h': 'c',
-  'eex': 'elixir',
-  'ex': 'elixir',
-  'exs': 'elixir',
-  'xls': 'excel',
-  'xlsb': 'excel',
-  'xlsx': 'excel',
-  'xlt': 'excel',
-  'xlsm': 'excel',
-  'es6': 'js',
-  'js.erb': 'js',
-  'js.flow': 'js'
+var fileIcons = [
+  { icon: 'git',
+    files:['.gitignore', '.gitattributes', '.gitmodules'],
+    extensions: []
+  },
+  { icon: 'bower',
+    files:['bower.json', '.bowerrc', 'bowerfile'],
+    extensions: []
+  },
+  { icon: 'babel',
+    files:['.babelrc'],
+    extensions: ['babel.js']
+  },
+  { icon: 'npm',
+    files:['package.json'],
+    extensions: []
+  },
+  { icon: 'nginx',
+    files:['nginx.conf'],
+    extensions: []
+  },
+  { icon: 'vagrant',
+    files:['Vagrantfile'],
+    extensions: []
+  },
+  { icon: 'docker',
+    files:['docker-compose.extra.yml', 'docker-compose.yml', '.dockerignore',
+           '.dockerignore', 'dockerfile', 'dockerfile.extra', 'docker-sync.yml'],
+    extensions: []
+  },
+  { icon: 'gulp',
+    files:['gulpfile.js'],
+    extensions: []
+  },
+  { icon: 'grunt',
+    files:['gruntfile.js'],
+    extensions: []
+  },
+  {
+    icon: 'html5',
+    files:[],
+    extensions: ['html', 'html.erb', 'slim']
+  },
+  {
+    icon: 'css3',
+    files:[],
+    extensions: ['css', 'less']
+  },
+  {
+    icon: 'sass',
+    files:[],
+    extensions: ['scss', 'sass']
+  },
+  {
+    icon: 'jsonld',
+    files:[],
+    extensions: ['json', 'jsonld', 'json']
+  },
+  {
+    icon: 'key',
+    files:[],
+    extensions: ['pub']
+  },
+  {
+    icon: 'image',
+    files:[],
+    extensions: ['png', 'jpg', 'svg']
+  },
+  {
+    icon: 'font',
+    files: [],
+    extensions: ['woff2', 'woff', 'ttf', 'ttc', 'otf', 'eot']
+  },
+  {
+    icon: 'access',
+    files:[],
+    extensions: ['accda', 'accde', 'accdb', 'accdr', 'accdt', 'adn', 'laccdb', 'mdw']
+  },
+  {
+    icon: 'asp',
+    files:[],
+    extensions: ['asax', 'ascx', 'ashx', 'asmx', 'aspx', 'axd']
+  },
+  {
+    icon: 'markdown',
+    files:[],
+    extensions: ['md']
+  },
+  {
+    icon: 'mustache',
+    files:[],
+    extensions: ['hbs']
+  },
+  {
+    icon: 'haskell',
+    files:[],
+    extensions: ['hs']
+  },
+  {
+    icon: 'clojure',
+    files:[],
+    extensions: ['clj']
+  },
+  {
+    icon: 'cpp',
+    files:[],
+    extensions: ['hpp', 'cpp']
+  },
+  {
+    icon: 'c',
+    files:[],
+    extensions: ['c', 'h']
+  },
+  {
+    icon: 'elixir',
+    files:[],
+    extensions: ['eex', 'ex', 'exs']
+  },
+  {
+    icon: 'excel',
+    files:[],
+    extensions: ['xls', 'xlsb', 'xlsx', 'xlt', 'xlsm']
+  },
+  {
+    icon: 'js',
+    files:[],
+    extensions: ['js', 'es6', 'js.erb', 'js.flow']
+  },
+
+];
 
 
+var fileNamesToClass = {};
+var extentionToClass = {};
+for(var i = 0; i < fileIcons.length; i++){
+  var icon =  fileIcons[i];
+  for(var j = 0; j < icon.files.length; j++){
+    fileNamesToClass[icon.files[j]] = icon.icon;
+  }
+  for(var j = 0; j < icon.extensions.length; j++){
+    extentionToClass[icon.extensions[j]] = icon.icon;
+  }
 
-};
+}
+
+
 
 
 
